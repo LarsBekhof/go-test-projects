e621b0116b9fc06cc21501c1e6fd85c7898ec39b
Lars Bekhof <larsbekhof@gmail.com>
2025-08-25 23:03:02 +0200

    feat: added websocket

--- chat/chat.go
+++ chat/chat.go
@@ -4,20 +4,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"github.com/gorilla/websocket"
 )
 
-func onFatalErr(err any) {
-	fmt.Println(err)
-	os.Exit(1)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+	HandshakeTimeout: 0,
+	ReadBufferSize: 0,
+	WriteBufferSize: 0,
+	WriteBufferPool: nil,
+	Subprotocols: []string{},
+	Error: func(w http.ResponseWriter, r *http.Request, status int, reason error) {
+		panic("TODO")
+	},
+	EnableCompression: false,
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("assets")))
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/static/", handleAssets)
+	http.HandleFunc("/ws/", handleWs)
 
 	port := ":9000"
 
 	fmt.Println("Server is running on port " + port)
 
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func handleAssets(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.RequestURI[1:])
+}
+
+func handleWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		panic("Something went wrong")
+	}
+	defer ws.Close()
+
+	for {
+		_, msg, err := ws.ReadMessage()
+
+		if err != nil {
+			panic("Something went wrong")
+		}
+
+		if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil {
+			panic("Something went wrong")
+		}
+	}
 }
--- chat/go.mod
+++ chat/go.mod
@@ -1,3 +1,5 @@
 module larsbekhof/chat
 
 go 1.22.2
+
+require github.com/gorilla/websocket v1.5.3 // indirect
